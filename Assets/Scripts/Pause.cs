51d670261f475c9f2d4f019dc420a8bb744c9507
Kimjimin0807 <jm004471@naver.com>
2020-12-05 01:02:31 +0900

    Timer Scenechang Pause UI

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    private bool _isPause;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _isPause = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PauseGame()
+    {
+        if (_isPause == false)
+        {
+            Time.timeScale = 0;
+            _isPause = true;
+            return; 
+        }
+
+        if (_isPause == true)
+        {
+            Time.timeScale = 1;
+            _isPause = false;
+            return;
+        }
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text TimeText;    //타이머 출력용 텍스트
+    public float GameTime; //타이머, 유니티 안에서 기준시간 선언
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameTime > 0)
+            GameTime -= Time.deltaTime;
+
+        TimeText.text = "Time : " + Mathf.Ceil(GameTime).ToString();
+    }
+}
