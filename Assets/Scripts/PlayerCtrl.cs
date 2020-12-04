190cd75633b599a4fbb7f4e8e11d9c05616f3948
tyrosine1153 <tyrosine1153@gmail.com>
2020-12-05 00:54:05 +0900

    Add PlayerCtrl script
    moveSpeed 일정하게 커지는 속도
    maxSpeed 최대 속도
    stopPointX 정지하는 구간
    stopDistance 정지하는 구간의 길이
    
    진행 로직 : 일정한 속도로 x축 방향으로 이동 -> 퍼즐이 있는 구간에 도달하면 멈춤 -> 퍼즐을 푼 상태가 되면 다시 직진함

---
+++ Assets/Scripts/PlayerCtrl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 유정민 작성
+public class PlayerCtrl : MonoBehaviour
+{
+    public int moveSpeed; // 보통 이동 속도
+    public int maxSpeed; // 최대 이동 속도
+    public int stopPointX; // 이동 중 퍼즐 구간에 왔을 때
+    public int stopDistance; // 퍼즐 구간 거리
+
+    private Rigidbody2D rigidbody;
+    private bool isSolved; // 퍼즐을 품
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        isSolved = false;
+    }
+
+    void Update()
+    {
+        if (rigidbody.velocity.x > maxSpeed) // 최대 이동 속도로 속도 고정
+            rigidbody.velocity = new Vector2(maxSpeed, rigidbody.velocity.y);
+
+        if (stopPointX < transform.position.x && transform.position.x < stopPointX + stopDistance && !isSolved)
+        {
+            // 구간에 도달 했을 때 멈춤
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x * 0.1f, rigidbody.velocity.y);
+        }
+        else
+            rigidbody.AddForce(Vector2.right * moveSpeed, ForceMode2D.Impulse);
+
+
+        // 대충 문제 풀고 isSolved = true 로 만드는 코드 (SetIsSolved())
+
+        if (transform.position.x > stopPointX + stopDistance) // 다시 이동 시작하고 구간을 벗어나면 다음 퍼즐 준비
+            isSolved = false;
+    }
+    
+    // 디버그/테스트용 메서드.
+    public void SetIsSolved()
+    {
+        isSolved = true;
+    }
+}
