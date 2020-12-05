dc5e91f06aaecb7f94848fd0e12ee3aa4968ccfd
YoonBin <yoonbin13@naver.com>
2020-12-05 23:28:19 +0900

    Stage Setting

---
+++ Assets/Scripts/GameDirector.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameDirector : MonoBehaviour
+{
+    public GameObject startbt;
+    public float DelayTime;
+    
+    private void Start()
+    {
+        Invoke("touchinvoke", DelayTime);
+
+        DontDestroyOnLoad(this);
+    }
+    void touchinvoke()
+    {
+        startbt.gameObject.SetActive(true);
+    }
+
+    public void MoveToStageScene()
+    {
+        SceneManager.LoadScene("StageScene");
+    }
+    public void MoveToStage1()
+    {
+        SceneManager.LoadScene("Stage1");
+    }
+
+}
