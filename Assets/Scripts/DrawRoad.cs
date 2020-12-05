f42ca492f66384b4e04002c72d65882eac4ecaa0
YoonBin <yoonbin13@naver.com>
2020-12-05 19:33:01 +0900

    Building Setting

--- Assets/Scripts/DrawRoad.cs
+++ Assets/Scripts/DrawRoad.cs
@@ -2,24 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DrawLine : MonoBehaviour
+public class DrawRoad : MonoBehaviour
 {
     public float            drawableAmount = 1000.0f;
 
-    public GameObject       trailPrefab;
-    public GameObject       currentTrail;
+    public GameObject       roadPrefab;
+    public GameObject       currentRoad;
 
-    public List<Vector2>    trailPointList;
+    public List<Vector2>    roadPointList;
 
 
     LineRenderer            _lineRenderer;
-    EdgeCollider2D          _edgeCollider2D;
-    Rigidbody2D             _trailRigidbody2D;
+    PolygonCollider2D       _polygonCollider2D;
+    Rigidbody2D             _roadRigidbody2D;
 
     public void Initialized()
     {
-        _trailRigidbody2D = trailPrefab.GetComponent<Rigidbody2D>();
-        _trailRigidbody2D.gravityScale = 0;
+        _roadRigidbody2D = roadPrefab.GetComponent<Rigidbody2D>();
+        _roadRigidbody2D.gravityScale = 0;
     }
     
     private void Start()
@@ -29,7 +29,7 @@ public class DrawLine : MonoBehaviour
 
     public void DestroyAllTrail()
     {
-        GameObject[] trails = GameObject.FindGameObjectsWithTag("Trail");
+        GameObject[] trails = GameObject.FindGameObjectsWithTag("Road");
 
         foreach (GameObject trail in trails)
         {
@@ -64,7 +64,7 @@ public class DrawLine : MonoBehaviour
                 {
                     Vector2 tempFingerPos = rayCastHit.point;
 
-                    if (Vector2.Distance(tempFingerPos, trailPointList[trailPointList.Count - 1]) > 0.1f)
+                    if (Vector2.Distance(tempFingerPos, roadPointList[roadPointList.Count - 1]) > 0.1f)
                     {
                         UpdateLine(tempFingerPos);
                         drawableAmount--;
@@ -74,42 +74,36 @@ public class DrawLine : MonoBehaviour
 
             if (touch.phase == TouchPhase.Ended)
             {
-                currentTrail.GetComponent<Rigidbody2D>().gravityScale = 1;
-                //DestroyAllTrail();
+                currentRoad.GetComponent<Rigidbody2D>().gravityScale = 1;
             }
         }
     }
 
     void CreateLine(Vector2 point)
     {
-        currentTrail = Instantiate(trailPrefab, Vector3.zero, Quaternion.identity);
+        currentRoad = Instantiate(roadPrefab, Vector3.zero, Quaternion.identity);
 
-        _lineRenderer = currentTrail.GetComponent<LineRenderer>();
-        _edgeCollider2D = currentTrail.GetComponent<EdgeCollider2D>();
+        _lineRenderer = currentRoad.GetComponent<LineRenderer>();
+        _polygonCollider2D = currentRoad.GetComponent<PolygonCollider2D>();
 
-        trailPointList.Clear();
+        roadPointList.Clear();
 
-        trailPointList.Add(point);
-        trailPointList.Add(point);
+        roadPointList.Add(point);
+        roadPointList.Add(point);
 
-        _lineRenderer.SetPosition(0, trailPointList[0]);
-        _lineRenderer.SetPosition(1, trailPointList[1]);
+        _lineRenderer.SetPosition(0, roadPointList[0]);
+        _lineRenderer.SetPosition(1, roadPointList[1]);
 
-        _edgeCollider2D.points = trailPointList.ToArray();
+        _polygonCollider2D.points = roadPointList.ToArray();
 
     }
 
     void UpdateLine(Vector2 point)
     {
-        trailPointList.Add(point);
+        roadPointList.Add(point);
         _lineRenderer.positionCount++;
         _lineRenderer.SetPosition(_lineRenderer.positionCount - 1, point);
-        _edgeCollider2D.points = trailPointList.ToArray();
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        Debug.Log(collision.gameObject.name);
+        _polygonCollider2D.points = roadPointList.ToArray();
     }
 }
     
