d65203581ceaf168ca61bc022e9690b6ee43dc49
Kimjimin0807 <jm004471@naver.com>
2020-12-05 18:09:04 +0900

    save

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "goal")
+        {
+            ShowGoal();
+        }
+    }
+
+    private void ShowGoal()
+    {
+
+    }
+}
