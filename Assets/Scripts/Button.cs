5ce76f7d2b9c7b3a018fa32a3fc8c6b719d94bd8
Kimjimin0807 <jm004471@naver.com>
2020-12-05 17:13:49 +0900

    Add funtion
    if Button pushed Move foothold

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public bool isPushed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isPushed = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(isPushed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "key")
+        {
+            isPushed = true;
+        }
+    }
+}
